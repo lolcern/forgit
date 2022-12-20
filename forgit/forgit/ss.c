fd748913bed57c427b81635427ec44eab8592a60
lolcern <lolcernje@gmail.com>
2022-12-20 12:09:28 +0900

    wansung

--- forgit/forgit/ss.c
+++ forgit/forgit/ss.c
@@ -1,7 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
 
 int main() {
-	
+	srand(time(NULL));
+
+	for (int i = 0; i < 10;) {
+		int a = rand() % 19 + 1;
+		int b = rand() % 19 + 1;
+		int c;
+		int sa = rand() % 4;
+		printf("\n[%d/10]", i);
+		switch (sa) {
+		case 0: printf("%d + %d = ", a, b);
+			scanf("%d", &c);
+			if (a + b == c) {
+				printf("Correct!");
+				i++;
+				break;
+			}
+			else {
+				printf("Wrong!");
+				break;
+			}
+		case 1: printf("%d - %d = ", a, b);
+			scanf("%d", &c);
+			if (a - b == c) {
+				printf("Correct!");
+				i++;
+				break;
+			}
+			else {
+				printf("Wrong!");
+				break;
+			}
+		case 2: printf("%d * %d = ", a, b);
+			scanf("%d", &c);
+			if (a * b == c) {
+				printf("Correct!");
+				i++;
+				break;
+			}
+			else {
+				printf("Wrong!");
+				break;
+			}
+		case 3:printf("%d / %d = ", a, b);
+			scanf("%d", &c);
+			if (a / b == c) {
+				printf("Correct!");
+				i++;
+				break;
+			}
+			else {
+				printf("Wrong!");
+				break;
+			}
+		}
+	}
 }
